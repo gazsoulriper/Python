22901f750ee25e67f91017d084cf8615d47b7a7a
gazsoulriper <gazsoulriper@gmail.com>
2022-08-10 13:43:16 -0300

    aplicação de métodos em strings

---
+++ metodos_embutidos_strings.py
@@ -0,0 +1,25 @@
+# Quebre a string "Hello Python" em uma lista
+s = "Hello Python".split()
+print(s)
+
+# caixa alta
+s = "Hello Python".upper()
+print(s)
+
+# caixa baixa
+s = "Hello Python".lower()
+print(s)
+
+# concatenação
+s = "Hello Python"
+s = s + " concatenação"
+print(s)
+
+# multiplicação de string
+s = "z"
+m = s*10
+print(m)
+
+# divide em um elemento especifico sem inserir esse elemento
+s = "Hello Python".split("P")
+print(s)
