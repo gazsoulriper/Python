637ff910502b0ac65ee4c9e12d1a69ee62f5dc1b
gazsoulriper <gazsoulriper@gmail.com>
2022-08-10 09:56:50 -0300

    arquivo com várias operações aritméticas como exemplo

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,33 @@
+# soma
+soma = 2 + 1
+print(f'o resultado de 2 + 1 é {soma}')
+
+# subtração
+sub = 2 - 1
+print(f'o resultado de 2 - 1 é {sub}')
+
+# multiplicação
+multi = 2 * 5
+print(f'o resultado de 2 x 5 é {multi}')
+
+# divisão
+div = 16 / 4
+print(f'o resultado de 16 ÷ 4 é {div}')
+
+# potência
+pot = 2 ** 3
+print(f'o resultado de 2^3 é {pot}')
+
+# raiz quadrada
+raiz = 25 ** 0.5
+print(f'a raiz quadrada de ✓25 é {raiz}')
+
+# módulo resto da divisão
+mod = 10 % 3
+print(f'o resto da divisão de 10 ÷ 3 é {mod}')
+
+# atribuição de variáveis
+distancia = 100
+tempo = 20
+velocidade = distancia / tempo
+print(f'a velocidade definida para uma distância de 100m gastando 20s para percorre-lá é de  {velocidade} m/s')
