ce16a6671b3d60dea5171a9f20e4a3e2c0a34cad
gazsoulriper <gazsoulriper@gmail.com>
2022-08-12 12:45:09 -0300

    exercício utilizando formatação de impressão

---
+++ formatacao_de_impressao.py
@@ -0,0 +1,5 @@
+# utilize a formatação de impressão para imprimir a frase " O diâmetro da Terra é de 12742 kilômetros.
+planeta = "Terra"
+diametro = 12742
+
+print(f'O diâmetro da {planeta} é de {diametro} kilômetros.')
